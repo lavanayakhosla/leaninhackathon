ec77bf3a80f2f4cea5130ef7e9a605d500a4c9b8
lavanayakhosla <144594627+lavanayakhosla@users.noreply.github.com>
2025-02-01 09:55:29 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,9 +18,7 @@ const Navbar = () => {
         {["NGOs", "Success Stories", "Donate"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
-        <li className="text-white bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
-        </li>
+       
       </ul>
       <div className="flex relative">
         {!toggleMenu && (
